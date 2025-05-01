52e3729c7d65b1957af6e410c2a86e39b39ded4f
smtdfc <smtdfc.master@gmail.com>
2025-05-01 12:15:41 +0700

    update health.js

--- netlify/functions/health.js
+++ netlify/functions/health.js
@@ -1,13 +1,25 @@
 exports.handler = async (event, context) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204, 
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+      body: '',
+    };
+  }
+  
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', 
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', 
-      'Access-Control-Allow-Headers': 'Content-Type', 
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
     body: JSON.stringify({
-      text: "ok"
+      text: "ok",
     }),
   };
 };
